1549742db598f4162b86aad1724be93dbf0e0feb
SongGayoung <xjongam1x@gmail.com>
2023-06-27 14:09:34 +0900

    즉시실행함수 적용

--- js/clock.js
+++ js/clock.js
@@ -1,37 +1,39 @@
-const dateElement = document.getElementById("date");
-const timeElement = document.getElementById("time");
+(function () {
+  const dateElement = document.getElementById("date");
+  const timeElement = document.getElementById("time");
 
-//날짜의 숫자가 한 자리수 일때, 앞에 0을 붙이는 함수
-const modifyNumber = (number) => {
-  return parseInt(number) < 10 ? "0" + number : number;
-};
+  //날짜의 숫자가 한 자리수 일때, 앞에 0을 붙이는 함수
+  const modifyNumber = (number) => {
+    return parseInt(number) < 10 ? "0" + number : number;
+  };
 
-const getNowDate = () => {
-  const nowDate = new Date();
-  const week = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-  let month = modifyNumber(nowDate.getMonth() + 1);
-  let date = modifyNumber(nowDate.getDate());
-  let day = nowDate.getDay();
-  setNowDate(month, date, week[day]);
-};
+  const getNowDate = () => {
+    const nowDate = new Date();
+    const week = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    let month = modifyNumber(nowDate.getMonth() + 1);
+    let date = modifyNumber(nowDate.getDate());
+    let day = nowDate.getDay();
+    setNowDate(month, date, week[day]);
+  };
 
-const setNowDate = (month, date, day) => {
-  dateElement.textContent = `${month}월 ${date}일 ${day}`;
-};
+  const setNowDate = (month, date, day) => {
+    dateElement.textContent = `${month}월 ${date}일 ${day}`;
+  };
 
-const getNowTime = () => {
-  const nowDate = new Date();
-  let hours = modifyNumber(nowDate.getHours());
-  let minutes = modifyNumber(nowDate.getMinutes());
-  let seconds = modifyNumber(nowDate.getSeconds());
-  setNowTime(hours, minutes, seconds);
-};
+  const getNowTime = () => {
+    const nowDate = new Date();
+    let hours = modifyNumber(nowDate.getHours());
+    let minutes = modifyNumber(nowDate.getMinutes());
+    let seconds = modifyNumber(nowDate.getSeconds());
+    setNowTime(hours, minutes, seconds);
+  };
 
-const setNowTime = (hours, minutes, seconds) => {
-  timeElement.textContent = `${hours} : ${minutes} : ${seconds}`;
-};
+  const setNowTime = (hours, minutes, seconds) => {
+    timeElement.textContent = `${hours} : ${minutes} : ${seconds}`;
+  };
 
-getNowDate();
-getNowTime();
-//시간이 1초마다 실행되는 함수
-setInterval(getNowTime, 1000);
+  getNowDate();
+  getNowTime();
+  //시간이 1초마다 실행되는 함수
+  setInterval(getNowTime, 1000);
+})();
--- js/quote.js
+++ js/quote.js
@@ -1,41 +1,43 @@
-const API_URL =
-  "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
-const quoteElement = document.getElementById("quote");
-const quoteItem = localStorage.getItem("quote");
+(function () {
+  const API_URL =
+    "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
+  const quoteElement = document.getElementById("quote");
+  const quoteItem = localStorage.getItem("quote");
 
-const nowDate = new Date();
-const month = nowDate.getMonth() + 1;
-const date = nowDate.getDate();
+  const nowDate = new Date();
+  const month = nowDate.getMonth() + 1;
+  const date = nowDate.getDate();
 
-//명언이 하루에 한번씩만 리셋되게 하기
+  //명언이 하루에 한번씩만 리셋되게 하기
 
-const setQuote = (result) => {
-  let quote = { createDate: `${month}-${date}`, quoteData: result };
-  localStorage.setItem("quote", JSON.stringify(quote));
-  quoteElement.textContent = result;
-};
+  const setQuote = (result) => {
+    let quote = { createDate: `${month}-${date}`, quoteData: result };
+    localStorage.setItem("quote", JSON.stringify(quote));
+    quoteElement.textContent = result;
+  };
 
-//명언 데이터 받아오기 api
-const getQuote = async () => {
-  try {
-    const data = await fetch(API_URL).then((res) => res.json());
-    const result = data[1].respond;
-    setQuote(result);
-  } catch (err) {
-    console.log(`err:${err}`);
-    setQuote(
-      "정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 한다. - 찰리 채플린"
-    );
-  }
-};
+  //명언 데이터 받아오기 api
+  const getQuote = async () => {
+    try {
+      const data = await fetch(API_URL).then((res) => res.json());
+      const result = data[1].respond;
+      setQuote(result);
+    } catch (err) {
+      console.log(`err:${err}`);
+      setQuote(
+        "정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 한다. - 찰리 채플린"
+      );
+    }
+  };
 
-if (quoteItem) {
-  let { createDate, quoteData } = JSON.parse(quoteItem);
-  if (createDate === `${month}-${date}`) {
-    quoteElement.textContent = `"${quoteData}"`;
+  if (quoteItem) {
+    let { createDate, quoteData } = JSON.parse(quoteItem);
+    if (createDate === `${month}-${date}`) {
+      quoteElement.textContent = `"${quoteData}"`;
+    } else {
+      getQuote();
+    }
   } else {
     getQuote();
   }
-} else {
-  getQuote();
-}
+})();
