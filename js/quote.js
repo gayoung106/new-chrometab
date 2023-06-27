5b5641f66008504cfdac6247c19968dfa1008504
SongGayoung <xjongam1x@gmail.com>
2023-06-27 12:26:25 +0900

    하루마다 명언 리셋

--- js/quote.js
+++ js/quote.js
@@ -1,12 +1,21 @@
 const API_URL =
   "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
 const quoteElement = document.getElementById("quote");
+const quoteItem = localStorage.getItem("quote");
+
+const nowDate = new Date();
+const month = nowDate.getMonth() + 1;
+const date = nowDate.getDate();
 
 //명언이 하루에 한번씩만 리셋되게 하기
 
 const setQuote = (result) => {
+  let quote = { createDate: `${month}-${date}`, quoteData: result };
+  localStorage.setItem("quote", JSON.stringify(quote));
   quoteElement.textContent = result;
 };
+
+//명언 데이터 받아오기 api
 const getQuote = async () => {
   try {
     const data = await fetch(API_URL).then((res) => res.json());
@@ -15,9 +24,18 @@ const getQuote = async () => {
   } catch (err) {
     console.log(`err:${err}`);
     setQuote(
-      "진정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 한다. - 찰리 채플린"
+      "정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 한다. - 찰리 채플린"
     );
   }
 };
 
-getQuote();
+if (quoteItem) {
+  let { createDate, quoteData } = JSON.parse(quoteItem);
+  if (createDate === `${month}-${date}`) {
+    quoteElement.textContent = `"${quoteData}"`;
+  } else {
+    getQuote();
+  }
+} else {
+  getQuote();
+}
