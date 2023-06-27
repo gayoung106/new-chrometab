f85c6c71060293cf6c60b6a04ce9fd8e17c4bc19
SongGayoung <xjongam1x@gmail.com>
2023-06-27 13:23:43 +0900

    early return 적용

--- js/bookmark-toggle.js
+++ js/bookmark-toggle.js
@@ -4,19 +4,12 @@ const bookmarkClose = document.getElementById("bookmark-close");
 
 const bookmarkBarToggle = () => {
   const isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen");
-
-  if (isBookMarkBarOpen) {
-    if (isBookMarkBarOpen === "open") {
-      localStorage.setItem("isBookMarkBarOpen", "close");
-      bookmarkBar.style.display = "none";
-      bookmarkOpen.style.display = "none";
-      bookmarkClose.style.display = "flex";
-    } else {
-      localStorage.setItem("isBookMarkBarOpen", "open");
-      bookmarkBar.style.display = "block";
-      bookmarkOpen.style.display = "flex";
-      bookmarkClose.style.display = "none";
-    }
+  if (isBookMarkBarOpen === "close") {
+    localStorage.setItem("isBookMarkBarOpen", "open");
+    bookmarkBar.style.display = "block";
+    bookmarkOpen.style.display = "flex";
+    bookmarkClose.style.display = "none";
+    return;
   } else {
     localStorage.setItem("isBookMarkBarOpen", "close");
     bookmarkBar.style.display = "none";
