6db0386d95f4f79116ee4cf6adb074c7700592b2
SongGayoung <xjongam1x@gmail.com>
2023-06-27 11:03:09 +0900

    명언 css

--- js/quote.js
+++ js/quote.js
@@ -2,6 +2,8 @@ const API_URL =
   "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
 const quoteElement = document.getElementById("quote");
 
+//명언이 하루에 한번씩만 리셋되게 하기
+
 const setQuote = (result) => {
   quoteElement.textContent = result;
 };
@@ -12,6 +14,9 @@ const getQuote = async () => {
     setQuote(result);
   } catch (err) {
     console.log(`err:${err}`);
+    setQuote(
+      "진정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 한다. - 찰리 채플린"
+    );
   }
 };
 
